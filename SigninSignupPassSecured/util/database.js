57fed156ea8e5bc086ae195519abe970edbb52ef
nawab55 <mdnawab3674@gmail.com>
2024-01-25 13:27:56 +0530

    User's password is at risk!!!! que6

---
+++ SigninSignupPassSecured/util/database.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('expense-tracker', 'root', 'Nawab@#$123', {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+// Test the database connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+module.exports = sequelize;
