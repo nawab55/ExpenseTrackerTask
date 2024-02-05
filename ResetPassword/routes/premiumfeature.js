9cf1e1aafa95f0ae415b42c6484b65f50f18d304
nawab55 <mdnawab3674@gmail.com>
2024-02-05 16:52:57 +0530

    Sending Reset Password Link  que15

---
+++ ResetPassword/routes/premiumfeature.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const premiumFeatureController = require('../controllers/premiumFeature');
+const authenticatemiddleware = require('../middleware/auth');
+
+const router = express.Router();
+
+router.get('/showLeaderBoard', authenticatemiddleware.authenticate, premiumFeatureController.getUserLeaderBoard);
+
+module.exports = router;
